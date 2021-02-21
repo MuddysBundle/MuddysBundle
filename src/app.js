eae9496ea06e47948a8d16a60c5a84626a555895
Beatso <39217836+Beatso1@users.noreply.github.com>
2021-02-21 22:34:29 +0000

    change port

--- src/app.js
+++ src/app.js
@@ -221,7 +221,7 @@ app.post("/uploadpack", (req, res) => {
 
 
 // listen server with express
-const server = app.listen(process.env.PORT || 3000, () => console.log("Server running"))
+const server = app.listen(process.env.PORT || 1300, () => console.log("Server running"))
 
 // export express stuff for testing
 module.exports.app = app
