6bf8f25b49d993d03aa91db095ee5136541b5e77
Beatso <39217836+Beatso@users.noreply.github.com>
2021-05-11 18:20:56 +0100

    clicking logo redirects to home - fixes #33

--- public/js/global.js
+++ public/js/global.js
@@ -2,3 +2,7 @@ document.getElementById('nose-map').onclick = () => {
 	const audio = new Audio('/honk.mp3')
 	audio.play()
 }
+
+document.getElementById('site-logo').onclick = () => {
+	window.location.href = '/'
+}
