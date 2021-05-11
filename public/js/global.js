aad7e0c343a8a0aeb3b08b6e7e7a2b98a261c7aa
Beatso <39217836+Beatso@users.noreply.github.com>
2021-05-11 18:16:20 +0100

    remove unnecessary logging

--- public/js/global.js
+++ public/js/global.js
@@ -1,8 +1,4 @@
-console.log(document.getElementById("nose-map"))
-
 document.getElementById('nose-map').onclick = () => {
 	const audio = new Audio('/honk.mp3')
 	audio.play()
 }
-
-console.log(document.getElementById('nose-map').onclick)
